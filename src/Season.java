7f7f6f088b1be0983ef9e36d21bd0a90411ee9e2
JAEJUN0124 <jaejuno123w@gmail.com>
2024-03-29 15:41:12 +0900

    2장 예제문

--- src/Season.java
+++ src/Season.java
@@ -7,15 +7,15 @@ public class Season {
         int month = scanner.nextInt(); // 정수로 월 입력
 
         if(3 <= month && month <= 5) {
-            System.out.println("봄입니다");
+            System.out.println("봄입니다.");
         }
         else if(6 <= month && month <= 8) {
-            System.out.println("여름입니다");
+            System.out.println("여름입니다.");
         }
         else if(9 <= month && month <= 11) {
-            System.out.println("가을입니다");
+            System.out.println("가을입니다.");
         }
         else {
-            System.out.println("겨울입니다");
+            System.out.println("겨울입니다.");
         }
 }
