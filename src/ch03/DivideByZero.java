d00eebbfedf0ae1a7c3a09ab8e680339f6e91d0a
JAEJUN0124 <jaejuno123w@gmail.com>
2024-04-05 16:29:36 +0900

    4장 내용 정리

--- src/ch03/DivideByZero.java
+++ src/ch03/DivideByZero.java
@@ -10,6 +10,13 @@ public class DivideByZero {
         dividend = scanner.nextInt(); // 나뉨수 입력
         System.out.print("나눗수를 입력하세요:");
         divisor = scanner.nextInt(); // 나눗수 입력
-        System.out.println(dividend+"를 " + divisor + "로 나누면 몫은 " + dividend/divisor + "입니다.");
+        try {
+            System.out.println(dividend+"를 " + divisor + "로 나누면 몫은 " + dividend/divisor + "입니다.");
+        } catch (Exception e) { // ArithmeticException 에외 처리 코드
+            System.out.println("0으로 나눌 수 없습니다!");
+        }
+        finally {
+            scanner.close(); // 정상적이든 예외가 발생하든 최종적으로 scanner를 닫는다
+        }
     }
 }
